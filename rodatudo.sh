d391ef5983708e3df43ed892fc455db129f5eba2
danilobd <dbd235@yahoo.com>
2018-06-20 14:10:07 -0300

    Adicionado arquivos sh para automarizar

---
+++ rodatudo.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+## TESTE 1
+
+	php index.php 1 1
+	echo
+	php index.php 1 2
+	echo
+	php index.php 1 3
+	echo
+	php index.php 1 4
+	echo
+	php index.php 1 5
+	echo
+
+## TESTE 50
+
+	php index.php 50 1
+	echo
+	php index.php 50 2
+	echo
+	php index.php 50 3
+	echo
+	php index.php 50 4
+	echo
+	php index.php 50 5
+	echo
+
+## TESTE 100
+
+	php index.php 100 1
+	echo
+	php index.php 100 2
+	echo
+	php index.php 100 3
+	echo
+	php index.php 100 4
+	echo
+	php index.php 100 5
+	echo
+
+## TESTE 500
+
+	php index.php 500 1
+	echo
+	php index.php 500 2
+	echo
+	php index.php 500 3
+	echo
+	php index.php 500 4
+	echo
+	php index.php 500 5
+	echo
+
+## TESTE 1.000
+
+	php index.php 1000 1
+	echo
+	php index.php 1000 2
+	echo
+	php index.php 1000 3
+	echo
+	php index.php 1000 4
+	echo
+	php index.php 1000 5
+	echo
